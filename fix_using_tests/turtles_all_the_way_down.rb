fd7992511610f98d0f1a6d5d3a471fff5aba3eae
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-16 21:32:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,8 +1,7 @@
-
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
@@ -10,7 +9,7 @@ end
 
 def turtle_traits(turtles)
   turtles.map do |turtle|
-    turtles[:traits].each do |trait|
+    turtle[:traits].each do |trait|
       trait
     end
   end
